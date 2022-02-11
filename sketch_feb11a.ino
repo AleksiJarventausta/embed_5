be1b8dac5064c17e4efae0ed23a4bc6c7a73936e
Aleksi Järventausta <aksu.j@hotmail.com>
2022-02-11 10:30:46 +0200

    init

---
+++ sketch_feb11a.ino
@@ -0,0 +1,53 @@
+#include <LiquidCrystal.h>
+enum states {
+  start,
+  waiting,
+  measuring,
+  cheater
+};
+states state = start;
+const int button_pin = 2,
+button_debounce = 300;
+unsigned long measurement_start, result;
+volatile unsigned long button_press_time;
+volatile boolean button_pressed = false;
+const int rs = 12, en = 11, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+void setup() {
+  pinMode(button_pin, INPUT_PULLUP);
+  Serial.begin(9600);
+  attachInterrupt(
+    digitalPinToInterrupt(button_pin),
+    button_interrupt,
+    FALLING
+  );
+   lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+  delay(1000);
+}
+void button_interrupt() {
+  button_press_time = millis();
+  button_pressed = true;
+}
+void loop() {
+  switch (state) {
+    case start:
+      Serial.println("Press button to start the game!");
+      while (!button_pressed) {}
+      delay(button_debounce);
+      state = waiting;
+      break;
+    case waiting:
+      break;
+    case measuring:
+      break;
+    case cheater:
+      break;
+    default:
+    // We shouldn’t reach this, if every state is defined
+      Serial.println("Default state reached :SSS");
+      break;
+  }
+  button_pressed = false;
+}
